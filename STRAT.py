8a857499b0524f8580ddb347de58914512f1948e
agustinazn <agustina.zein@epfl.ch>
2024-06-21 14:49:27 +0200

    Refactor final 6 + update main

--- STRAT.py
+++ STRAT.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from Data_handler import RF_COL
-from termcolor import colored
+from scipy.optimize import minimize
 
 VOL_TARGET = 0.1
 
@@ -12,43 +12,129 @@ def get_mean_std_sharpe_STRAT(data, return_col:str):
     sr = (mean - rf) / std
     return mean, std, sr
 
+# Calculate mean-variance optimal weights
+def mean_variance_weights(mean_returns, cov_matrix, RF_COL):
+    num_assets = len(mean_returns)
+    
+    # Calculate portfolio volatility given weights
+    def portfolio_volatility(weights):
+        return np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights)))
+    
+    # Minimize the negative Sharpe ratio = maximize positive Sharpe ratio
+    def negative_sharpe_ratio(weights):
+        port_return = np.dot(weights, mean_returns)
+        port_volatility = portfolio_volatility(weights)
+        return -(port_return - RF_COL) / port_volatility
+    
+    # Weights sum to 1
+    constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}
+    
+    # Common initial guess for weights (equal weights)
+    init_guess = num_assets * [1. / num_assets]
+    
+    result = minimize(negative_sharpe_ratio, init_guess, constraints=constraints)
+    
+   
+    return result.x
+
+
 def run_strat_part6(data, returns_BAB, returns_MOM, returns_IV, question_a=True, question_b=True, show_plot = False, verbose=True):
     # Get the value weighted returns for each of the strategies implemented
-    dataBAB = returns_BAB[['date', 'ret']].rename(columns={'ret': 'rBAB'})
-    dataMOM = returns_MOM[['date', 'ret']].rename(columns={'ret': 'rMOM'})
-    dataIV = returns_IV['IV_question_c']['IV_question_c_VW_returns_data'][['date', 'ret']].rename(columns={'ret': 'rIV'})
+    dataBAB = returns_BAB['BAB_question_cd']['BAB_qc_strategy_data'][['date', 'rBAB']]#.rename(columns={'ret': 'rBAB'})
+    print(dataBAB)
+    dataMOM = returns_MOM['MOM_question_b']['MOM_qb_VW_returns'][['date', 'VW_ret', RF_COL]].rename(columns={'VW_ret': 'rMOM'})
+    print(dataMOM)
+    dataIV = returns_IV['IV_question_c']['IV_question_c_VW_returns_data'][['date', 'VW_ret']].rename(columns={'VW_ret': 'rIV'})
+    print(dataIV)
 
     # Merge the datasets into one 
     dataSTRAT = pd.merge(dataBAB, dataMOM, on='date', how='inner')
     dataSTRAT = pd.merge(dataSTRAT, dataIV, on='date', how='inner')
-    dataSTRAT= dataSTRAT[['date', RF_COL, 'rBAB', 'rMOM', 'rIV']]
+    print(dataSTRAT)
+    print(dataSTRAT.columns)
+    dataSTRAT = dataSTRAT[['date', RF_COL, 'rBAB', 'rMOM', 'rIV']]
+    print(dataSTRAT)
    
+
     ## EQUALLY WEIGHTED 
-    dataSTRAT['rSTRAT_EW'] = (dataSTRAT['rBAB'] + dataSTRAT['rMOM'] + dataSTRAT['rIV']) / 3
+    dataSTRAT_EW = dataSTRAT.copy()
+    dataSTRAT_EW['rSTRAT_EW'] = (dataSTRAT_EW['rBAB'] + dataSTRAT_EW['rMOM'] + dataSTRAT_EW['rIV']) / 3
 
     # Determining the c constant for each year
-    dataSTRAT['rSTRATstd'] = dataSTRAT.groupby(dataSTRAT.date.dt.year)['rSTRAT_EW'].transform('std') * np.sqrt(12)
-    dataSTRAT['C'] = 0.1 / dataSTRAT['rSTRATstd']
-    dataSTRAT['rFUND_EW'] = dataSTRAT['C'] * dataSTRAT['rSTRAT_EW'] + dataSTRAT[RF_COL]
+    dataSTRAT_EW['rSTRATstd'] = dataSTRAT_EW.groupby(dataSTRAT_EW.date.dt.year)['rSTRAT_EW'].transform('std') * np.sqrt(12)
+    dataSTRAT_EW['C'] = VOL_TARGET / dataSTRAT_EW['rSTRATstd']
+    dataSTRAT_EW['rFUND_EW'] = dataSTRAT_EW['C'] * dataSTRAT_EW['rSTRAT_EW'] + dataSTRAT_EW[RF_COL]
 
     # Get the stats for the EW strategy
-    retSTRAT_EW, stdSTRAT_EW, srSTRAT_EW = get_mean_std_sharpe_STRAT(dataSTRAT, 'rFUND_EW')
+    retSTRAT_EW, stdSTRAT_EW, srSTRAT_EW = get_mean_std_sharpe_STRAT(dataSTRAT_EW, 'rFUND_EW')
 
-    print(colored("STRAT strategy based on equally weighted portfolios", attrs=['underline', 'bold'])) 
+    print("STRAT strategy based on equally weighted portfolios") 
     print(" - Expected return:\t {:.2f}".format(retSTRAT_EW))
     print(" - Standard deviation:\t {:.2f}".format(stdSTRAT_EW))
     print(" - Sharpe ratio:\t {:.2f}".format(srSTRAT_EW))
 
-    dataSTRAT.drop(columns=['rSTRAT_EW', 'rSTRATstd', 'C'])
+    dataSTRAT_EW.drop(columns=['rSTRAT_EW', 'rSTRATstd', 'C'], axis=1)
 
-    ### RISK PARITY 
-    dataSTRAT['rBABstd'] = dataSTRAT['rBAB'].rolling(window=36, min_periods=36, closed='left').std() * np.sqrt(12)
-    dataSTRAT['rMOMstd'] = dataSTRAT['rMOM'].rolling(window=36, min_periods=36, closed='left').std() * np.sqrt(12)
-    dataSTRAT['rIVstd'] = dataSTRAT['rIV'].rolling(window=36, min_periods=36, closed='left').std()   * np.sqrt(12)
-    dataSTRAT.dropna(inplace=True)
+
+
+    ### RISK PARITY
+    dataSTRAT_RP = dataSTRAT.copy()
+
+    dataSTRAT_RP['rBABstd'] = dataSTRAT_RP['rBAB'].rolling(window=36, min_periods=36, closed='left').std() * np.sqrt(12)
+    dataSTRAT_RP['rMOMstd'] = dataSTRAT_RP['rMOM'].rolling(window=36, min_periods=36, closed='left').std() * np.sqrt(12)
+    dataSTRAT_RP['rIVstd'] = dataSTRAT_RP['rIV'].rolling(window=36, min_periods=36, closed='left').std()   * np.sqrt(12)
+    dataSTRAT_RP.dropna(inplace=True)
 
     dataSTRAT_RP['rSTRAT'] = dataSTRAT_RP['rBAB']/dataSTRAT_RP['rBABstd'] + dataSTRAT_RP['rMOM']/dataSTRAT_RP['rMOMstd'] + dataSTRAT_RP['rIV']/dataSTRAT_RP['rIVstd']
 
-    dataSTRAT_RP.head()
-    
+    # Determining the c constant for each year
+    dataSTRAT_RP['rSTRATstd'] = dataSTRAT_RP.groupby(dataSTRAT_RP.date.dt.year)['rSTRAT'].transform('std') * np.sqrt(12)
+    dataSTRAT_RP['C'] = VOL_TARGET / dataSTRAT_RP['rSTRATstd']
+    dataSTRAT_RP['rFUND_RP'] = dataSTRAT_RP['C'] * dataSTRAT_RP['rSTRAT'] + dataSTRAT_RP[RF_COL]
+
+    retSTRAT_RP, stdSTRAT_RP, srSTRAT_RP = get_mean_std_sharpe_STRAT(dataSTRAT_RP, 'rFUND_RP')
+
+    print("STRAT strategy based on equally risk parity portfolios") 
+    print(" - Expected return:\t {:.2f}".format(retSTRAT_RP))
+    print(" - Standard deviation:\t {:.2f}".format(stdSTRAT_RP))
+    print(" - Sharpe ratio:\t {:.2f}".format(srSTRAT_RP))
+
+
+    ### MEAN VARIANCE OPTIMAL
+    dataSTRAT_MV = dataSTRAT.copy()
+    mean_MV = dataSTRAT_MV[['rBAB', 'rIV', 'rMOM']].rolling(window=36, min_periods=36, closed='left').mean() 
+    covariance_MV = dataSTRAT_MV[['rBAB', 'rIV', 'rMOM']].rolling(window=36, min_periods=36, closed='left').cov() 
+    dataSTRAT_MV.dropna(inplace=True)
+
+    optimal_weights_list = []
+    portfolio_returns = []
+
+    # Calculate optimal weights and portfolio returns for each rolling window
+    for i in range(36, len(dataSTRAT_MV)):
+        rolling_mean = mean_MV.iloc[i].values
+        rolling_cov = covariance_MV.iloc[i*3:(i+1)*3].values.reshape(3, 3)
+        
+        optimal_weights = mean_variance_weights(rolling_mean, rolling_cov, dataSTRAT_MV[RF_COL].iloc[i])
+        optimal_weights_list.append(optimal_weights)
+        
+        # Calculate portfolio return for the current period
+        portfolio_return = np.dot(optimal_weights, dataSTRAT_MV[['rBAB', 'rIV', 'rMOM']].iloc[i].values)
+        portfolio_returns.append(portfolio_return)
+
+    # Store the results 
+    #Ca on peut changer mais oke 
+    dataSTRAT_MV = dataSTRAT_MV.iloc[36:].copy()
+    dataSTRAT_MV['optimal_weights'] = optimal_weights_list
+    dataSTRAT_MV['rSTRAT_MV'] = portfolio_returns
+
+    # Determine the c constant for each year
+    dataSTRAT_MV['rSTRATstd'] = dataSTRAT_MV.groupby(dataSTRAT_MV.date.dt.year)['rSTRAT_MV'].transform('std') * np.sqrt(12)
+    dataSTRAT_MV['C'] = 0.1 / dataSTRAT_MV['rSTRATstd']
+    dataSTRAT_MV['rFUND_MV'] = dataSTRAT_MV['C'] * dataSTRAT_MV['rSTRAT_MV'] + dataSTRAT_MV[RF_COL]
+
+    retSTRAT_MV, stdSTRAT_MV, srSTRAT_MV = get_mean_std_sharpe_STRAT(dataSTRAT_MV, 'rFUND_MV')
+    print("STRAT strategy based on mean-variance optimal combination")
+    print(" - Expected return:\t {:.2f}".format(retSTRAT_MV))
+    print(" - Standard deviation:\t {:.2f}".format(stdSTRAT_MV))
+    print(" - Sharpe ratio:\t {:.2f}".format(srSTRAT_MV))
     return 0
--- main.py
+++ main.py
@@ -4,25 +4,23 @@ from Utils import SEP
 from BaB import run_bab_part3
 from Momentum import run_mom_part4
 from IV import run_iv_part5
+from STRAT import run_strat_part6
+#from Industry_Neutral import run_in_part8
 
 import os
 
 def main():
-
-    all_returns = dict()
 
     # Prepare data
     Data_instance = Data()               # create instance of the class Data()
     data = Data_instance.get_data()      # store the data of Data_instance
 
     data.sort_values(by=['permno','date'],inplace=True)
-
-    show = False
     
     # Add excess returns columns
     data['Rn_e'] = data['ret'] - data[RF_COL]
     data['Rm_e'] = data['vwretd'] - data[RF_COL]
-
+ 
     # Drop the NaN values
     # print('Initial shape of the data:', data.shape)
     data = data.dropna().copy()
@@ -32,27 +30,30 @@ def main():
     # print('Shape data:', data.shape)
     # print(data)
 
-    # 1 BAB
-    # print(f"{SEP} Running BaB {SEP}")
-    returns_BAB = run_bab_part3(data, question_a=True, question_b=True, question_cd=True, show_plot = show, save_tables=show, verbose=show)
-    all_returns['BAB'] = returns_BAB
+    # # 1 BAB
+    print(f"{SEP} Running BaB {SEP}")
+    returns_BAB = run_bab_part3(data, question_a=True, question_b=True, question_cd=True, save_tables=False, verbose=True)
 
-    # 2 MOM
+    # # 2 MOM
     print(f"{SEP} Running Mom {SEP}")
     returns_MOM = run_mom_part4(data, question_a=True, question_b=True, save_tables=False, verbose=True)
-    all_returns['MOM'] = returns_MOM
 
     # 3 IV
-    # print(f"{SEP} Running IV {SEP}")
-    returns_IV = run_iv_part5(data, question_a=True, question_b=True, question_c = True, show_plot = show, verbose=show)
+    print(f"{SEP} Running IV {SEP}")
+    returns_IV = run_iv_part5(data, question_a=True, question_b=True, show_plot = False, verbose=True)
+    
+    # 4 STRAT
+    print(f"{SEP} Running STRAT {SEP}")
+    returns_STRAT = run_strat_part6(data, returns_BAB, returns_MOM, returns_IV, question_a=True, question_b=True, show_plot = False, verbose=True)
 
-    all_returns['IV'] = returns_IV
+
+    # 8 Industry Neutral
+    #print(f"{SEP} Running Industry Neutral {SEP}")
+    #returns_IN = run_in_part8(data, question_a=True, question_b=False, save_tables=False, verbose=True)
 
     # Prepare the folder for the figures
     if not os.path.exists("Figures"):
         os.makedirs("Figures")
-
-    return all_returns
 
 if __name__ == "__main__":
     main()
