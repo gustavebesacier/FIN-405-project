82de316de152f25af3bea2ddf27410d8e8d45430
gustavebesacier <124517291+gustavebesacier@users.noreply.github.com>
2024-06-21 16:52:05 +0200

    main clean

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ import os
 
 def main():
 
+    all_returns = dict()
+
     # Prepare data
     Data_instance = Data()               # create instance of the class Data()
     data = Data_instance.get_data()      # store the data of Data_instance
 
     data.sort_values(by=['permno','date'],inplace=True)
-<<<<<<< Updated upstream
-=======
 
     # Check pour voir si ça résoud tous les problèmes - 
     data['N'] = data.groupby(['permno'])['date'].transform('count')
@@ -25,7 +25,6 @@ def main():
 
     show_verbose = True
     show = False
->>>>>>> Stashed changes
     
     # Add excess returns columns
     data['Rn_e'] = data['ret'] - data[RF_COL]
@@ -40,38 +39,26 @@ def main():
     # print('Shape data:', data.shape)
     # print(data)
 
-<<<<<<< Updated upstream
-    # # 1 BAB
-    print(f"{SEP} Running BaB {SEP}")
-    returns_BAB = run_bab_part3(data, question_a=True, question_b=True, question_cd=True, save_tables=False, verbose=True)
-=======
     # 1 BAB
     print(f"{SEP} Running BaB {SEP}")
     returns_BAB = run_bab_part3(data, question_a=True, question_b=True, question_cd=True, show_plot = show, save_tables=show, verbose=show_verbose)
     all_returns['BAB'] = returns_BAB
->>>>>>> Stashed changes
 
     # # 2 MOM
     print(f"{SEP} Running Mom {SEP}")
-<<<<<<< Updated upstream
-    returns_MOM = run_mom_part4(data, question_a=True, question_b=True, save_tables=False, verbose=True)
-
-    # 3 IV
-    print(f"{SEP} Running IV {SEP}")
-    returns_IV = run_iv_part5(data, question_a=True, question_b=True, show_plot = False, verbose=True)
-    
-    # 4 STRAT
-    print(f"{SEP} Running STRAT {SEP}")
-    data_STRAT_RP = run_strat_part6(data, returns_BAB, returns_MOM, returns_IV, question_a=True, question_b=True, show_plot = False, verbose=True)
-    ### In data_STRAT_RP, you can fetch: rBABstd, rMOMstd, rIVstd, rSTRAT (the weighted average of the strats), rSTRATstd, rFUND_RP
-=======
     returns_MOM = run_mom_part4(data, question_a=True, question_b=True, save_tables=False, show_plot = show, verbose=show_verbose)
     all_returns['MOM'] = returns_MOM
 
     # 3 IV
     print(f"{SEP} Running IV {SEP}")
+    print(f"{SEP} Running IV {SEP}")
     returns_IV = run_iv_part5(data, question_a=True, question_b=True, question_c = True, show_plot = show, verbose=show_verbose)
->>>>>>> Stashed changes
+    all_returns['IV'] = returns_IV
+    
+    # 4 STRAT
+    print(f"{SEP} Running STRAT {SEP}")
+    data_STRAT_RP = run_strat_part6(data, returns_BAB, returns_MOM, returns_IV, question_a=True, question_b=True, show_plot = False, verbose=True)
+    ### In data_STRAT_RP, you can fetch: rBABstd, rMOMstd, rIVstd, rSTRAT (the weighted average of the strats), rSTRATstd, rFUND_RP
 
     # 8 Industry Neutral
     #print(f"{SEP} Running Industry Neutral {SEP}")
@@ -80,6 +67,9 @@ def main():
     # Prepare the folder for the figures
     if not os.path.exists("Figures"):
         os.makedirs("Figures")
+    
+    print(all_returns.items())
+    print(all_returns)
 
 if __name__ == "__main__":
     main()
